dfa9d51bb86902e86f390c711c094f2667128eea
samatha <rasamatha281996@gmail.com>
2024-05-17 16:42:50 +0000

    Solution

--- app.js
+++ app.js
@@ -24,10 +24,7 @@ initializeAndStartDb()
 module.exports = app
 const convertDbObjectToResponseObject = dbobject => {
   return {
-    movieId: dbobject.movie_id,
-    directorId: dbobject.director_id,
     movieName: dbobject.movie_name,
-    leadActor: dbobject.lead_actor,
   }
 }
 //api1
@@ -50,10 +47,62 @@ app.post('/movies/', async (request, response) => {
   response.send('Movie Successfully Added')
 })
 //api3
-app.get('/movies/:movieId/', async (request, response) => {
+app.get('/movies/:movieId/', async(request, response) => {
   const {movieId} = request.params
   const getquery = `select * from movie
   where movie_id=${movieId} `
   dbresponse = await db.get(getquery)
   response.send(convertDbObjectToResponseObject(dbresponse))
+})
+//api4
+app.put('/movies/:movieId/', async (request, response) => {
+  const {movieId} = request.params
+  const moviedetails = request.body
+  const {directorId, movieName, leadActor} = moviedetails
+  const updatequery = `update movie
+  set director_id=${directorId},movie_name='${movieName}',lead_actor='${leadActor}' 
+  where movie_id=${movieId}`
+  await db.run(updatequery)
+  response.send('Movie Details Updated')
+})
+//api5
+app.delete('/movies/:movieId/', async (request, response) => {
+  const {movieId} = request.params
+  const deletequery = `delete from movie where movie_id=${movieId}`
+  await db.run(deletequery)
+  response.send('Movie Removed')
+})
+const convertintocamelcase = dbobject => {
+  return {
+    directorId: dbobject.director_id,
+    directorName: dbobject.director_name,
+  }
+}
+//api6
+app.get('/directors', async (request, response) => {
+  const directorquery = `select * from director`
+  dbresponse = await db.all(directorsquery)
+  response.send(
+    dbresponse.map(eachplayer => {
+      convertintocamelcase(eachplayer)
+    }),
+  )
+})
+module.exports = app
+//api7
+const getmovienames = dbobject => {
+  return {
+    movieName: dbobject.movie_name,
+  }
+}
+app.get('/directors/:directorsId/movies/', async (request, response) => {
+  const {directorId} = request.params
+  const moviequery = `select * from movie where 
+  director_id=${directorId}`;
+  dbresponse = await db.all(moviequery)
+  response.send(
+    dbresponse.map(eachmovie => {
+      getmovienames(eachmovie)
+    }),
+  )
 })
